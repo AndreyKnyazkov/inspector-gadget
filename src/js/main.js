4169c7a8f0ef8d1df10291f56178e35447a87fb7
Andrey Knyazkov <martinadnrey1998@gmail.com>
2020-09-27 16:50:32 +0300

    Skeleton html block of contacts was done

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,5 @@
-
-//Сначала по классу должен идти главный блок, заложенный в верстке, потом исполнительный класс скрипта, потом уже говорится закрыт он или нет
-//Пример: defect-tabs__block js-tab-main-block open
-//css код для скрипта: 
-/*
-.open .tab-js-subtitle {
-  -webkit-transform: scaleY(1);
-  transform: scaleY(1);
-  -webkit-transform-origin: top;
-  transform-origin: top;
-  -webkit-transition: -webkit-transform .4s ease;
-  transition: -webkit-transform .4s ease;
-  transition: transform .4s ease;
-  transition: transform .4s ease, -webkit-transform .4s ease;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-}
-
-.close .tab-js-subtitle {
-  height: 0;
-  -webkit-transition: height 1s ease-out;
-  transition: height 1s ease-out;
-  -webkit-transform: scaleY(0);
-  transform: scaleY(0);
-}
-*/
+// Отключить прокрутку яндекс карты руками
+//отключаем зум колёсиком мышки
 
 /* modal popup */
 $(document).ready(function($) {
