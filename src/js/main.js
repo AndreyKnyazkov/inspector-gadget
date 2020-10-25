4f0047fe0b4ec2a13172b2aa048d021569119e31
Andrey Knyazkov <martinadnrey1998@gmail.com>
2020-10-25 15:07:57 +0300

    add menu

--- src/js/main.js
+++ src/js/main.js
@@ -27,3 +27,85 @@ $(document).ready(function($) {
     }
   });
 });
+
+const menuBlockItem = document.querySelectorAll('.menu-block__item');
+let dataHover = document.querySelectorAll('.data-hover');
+
+menuBlockItem.forEach((elem, index, htmlelem) => {
+  //Find out all childs of the element
+  let allNodes = htmlelem[index].children,
+  //convert to array
+  arrNodes = Array.from(allNodes);
+  //mouseenter
+  elem.addEventListener('mouseenter', (e) => {
+    //iterate through the array find out data-attr
+    arrNodes.forEach((e, i, h) => {
+    if (e.classList.contains('data-hover')) {
+      //set new src
+      e.setAttribute('src', e.dataset.hover);
+      //change arrow to active
+      console.log(e.parentNode);
+      //show elements of menu
+      document.querySelectorAll('.menuitem-block').forEach((menu) => {
+        console.log(menu.getAttribute('data-link'));
+        if (menu.getAttribute('data-link') === e.parentNode.getAttribute('href')) {
+          menu.style.display = 'flex';
+        }
+      });
+      console.log(document.querySelectorAll('.menuitem-block'));
+      Array.from(e.parentNode.children).forEach(el => {
+        if (el.classList.contains('data-arrow')) {
+          el.setAttribute('src', el.dataset.hover);
+          el.classList.add('arrow-active');
+        }
+      });
+    }
+  });
+  });
+  //the same as was before but mouseleave
+  elem.addEventListener('mouseleave', (e) => {
+    arrNodes.forEach((e, i, h) => {
+    if (e.classList.contains('data-hover')) {
+      e.setAttribute('src', e.dataset.img);
+      document.querySelectorAll('.menuitem-block').forEach((menu) => {
+        console.log(menu.getAttribute('data-link'));
+        if (menu.getAttribute('data-link') === e.parentNode.getAttribute('href')) {
+          menu.style.display = 'none';
+        }
+      });
+      Array.from(e.parentNode.children).forEach(el => {
+        if (el.classList.contains('data-arrow')) {
+          el.setAttribute('src', el.dataset.img);
+          el.classList.remove('arrow-active');
+        }
+      });
+    }
+  });
+  });  
+});
+
+const menu = document.querySelector('.header-btn__burger-mobile'),
+      menuSrcOpen = menu.dataset.open,
+      menuSrcImg = menu.dataset.img,
+      menuMobile = document.querySelector('.menu-mobile');
+
+menu.addEventListener('click', (event) => {
+  if (menuMobile.style.display === 'block') {
+    menuMobile.setAttribute('style', 'display: none;');
+    menu.setAttribute('src', menuSrcImg);
+    document.querySelector('body').setAttribute('style', 'overflow: initial');
+  } else {
+    menuMobile.setAttribute('style', 'display: block;');
+    menu.setAttribute('src', menuSrcOpen);
+    window.scrollTo(0,0);
+    menuMobile.scrollTo(0,0);
+    document.querySelector('body').setAttribute('style', 'overflow: hidden');
+  }
+});
+
+window.addEventListener('resize', (event) => {
+  if (window.screen.width >= 1050 && window.screen.height <= 768 ) {
+  menuMobile.setAttribute('style', 'display: none');
+  menu.setAttribute('src', menuSrcImg);
+}
+});
--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,18 +1,20 @@
-const tabs = (mainBlock, titleTab, cssMain) => {
+const tabs = (mainBlock, titleTab, cssMain, numberOfOpenTab) => {
   //Главный блок с табом
-  const tabJsMainBlock = document.querySelectorAll(mainBlock),
+  const tabJsСssMain = document.querySelectorAll(cssMain),
     //Заголовок таба
     tabJsTitle = document.querySelectorAll(titleTab);
 
   //Добавляю ко всем элементом класс закрытия
   const closeTabs = () => {
     //закрываю все табы
-    tabJsMainBlock.forEach((elem) => {
+    tabJsСssMain.forEach((elem) => {
       elem.classList.add('close');
     });
     // Если нужно, добавляю элемент раскрытого таба
-    tabJsMainBlock[1].classList.remove('close');
-    tabJsMainBlock[1].classList.add('open');
+    if (numberOfOpenTab) {
+      tabJsСssMain[numberOfOpenTab].classList.remove('close');
+      tabJsСssMain[numberOfOpenTab].classList.add('open');
+    }    
   };
   // mainBlock, titleTab, cssMain
   closeTabs();
@@ -20,9 +22,8 @@ const tabs = (mainBlock, titleTab, cssMain) => {
   tabJsTitle.forEach((elem, i) => {
     elem.addEventListener('click', () => {
       const itemClass = elem.parentNode.className;
-      for (i = 0; i < tabJsMainBlock.length; i++) {
-        console.log(tabJsMainBlock.toString);
-        tabJsMainBlock[i].className = `${cssMain} ${mainBlock} close`.replace(/\./gi, '');
+      for (i = 0; i < tabJsСssMain.length; i++) {
+        tabJsСssMain[i].className = `${cssMain} ${mainBlock} close`.replace(/\./gi, '');
       }
       if (itemClass == `${cssMain} ${mainBlock} close`.replace(/\./gi, '')) {
         elem.parentNode.className = `${cssMain} ${mainBlock} open`.replace(/\./gi, '');
@@ -30,4 +31,21 @@ const tabs = (mainBlock, titleTab, cssMain) => {
     });
   });
 };
-tabs('.js-tab-main-block', '.tab-js-title', 'defect-tabs__block');
+//tabs
+tabs('.js-tab-main-block', '.tab-js-title', '.defect-tabs__block', 1);
+//menu
+tabs('.js-tab-main-block', '.tab-js-title', '.menu-mobile-block__item');
+
+/*
+var div = document.querySelector("#too"); //Это элемент от которого мы начнем поиск
+
+div.closest("#block"); //Результат - самый первый блок древа выше
+console.log('div.closest("#block"): ', div.closest("#block"));
+div.closest("div"); //Сам блок #too и будет результатом, так как он подходит под селектор "div"
+console.log('div.closest("div"): ', div.closest("div"));
+div.closest("a"); //null - В предках #too нет ни одного тега "a"!
+console.log('div.closest("a"): ', div.closest("a"));
+div.closest("div[title]") //#block - так как ближе нет блоков с атрибутом title.
+console.log('div.closest("div[title]"): ', div.closest("div[title]"));
+*/
+// console.log(document.querySelectorAll('.data-hover')[2]);
